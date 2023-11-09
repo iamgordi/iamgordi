717e1ccf3e6b1efe9bbc29585f40380c4873e17c
iamgordi <150351914+iamgordi@users.noreply.github.com>
2023-11-09 13:54:28 +0000

    one

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-function tree(){}
+function car(speed, length, mass){
+    let peed = speed;
+    let ength = length;
+    let ass = mass;
+    console.log(peed, ength, ass);
+    
+}
+car(10,10,200)
